99eff401c7511e414bb0e599a4ef12b38740291d
Young <fomagran6@naver.com>
2022-06-21 15:08:18 -0500

    Installed slugify Created ExerciseItem Added add Item and remove item in PlannerScreen

--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,13 +1,54 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import WorkoutForm, { ExerciseForm } from "../components/WorkoutForm";
+import { Sequence, SequenceType } from "../models/sequence";
+import slugify from "slugify";
+import { useState } from "react";
+import ExerciseItem from "../components/ExerciseItem";
+import { PressableText } from "../components/styled/PressableText";
 
 export default function PlannerScreen({ navigation }: any) {
+  const [seqItems, setSeqItems] = useState<Sequence[]>([]);
+
   const handleFormSubmit = (form: ExerciseForm) => {
-    alert(`${form.name} ${form.duration} ${form.type} ${form.reps}`);
+    const sequence: Sequence = {
+      slug: slugify(form.name + Date.now(), { lower: true }),
+      name: form.name,
+      type: form.type as SequenceType,
+      duration: Number(form.duration),
+    };
+
+    if (form.reps) {
+      sequence.reps = Number(form.reps);
+    }
+
+    setSeqItems([...seqItems, sequence]);
   };
   return (
-    <View>
+    <View style={styles.container}>
+      <FlatList
+        data={seqItems}
+        keyExtractor={(item) => item.slug}
+        renderItem={({ item, index }) => (
+          <ExerciseItem item={item}>
+            <PressableText
+              text="Remove"
+              onPressIn={() => {
+                const items = [...seqItems];
+                items.splice(index, 1);
+                setSeqItems(items);
+              }}
+            />
+          </ExerciseItem>
+        )}
+      />
       <WorkoutForm onSubmit={handleFormSubmit} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
