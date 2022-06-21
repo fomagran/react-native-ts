3e8edc77b448cced7c0d4234d4b6be2513040202
Young <fomagran6@naver.com>
2022-06-21 14:37:15 -0500

    Installed React-Hook-Form Applied react-hook-form to WorkoutForm

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import { PressableText } from "./styled/PressableText";
+import { useForm, Controller } from "react-hook-form";
 
 export type ExerciseForm = {
   name: string;
   duration: string;
+  type: string;
+  reps?: number;
 };
 
 type WorkoutProps = {
@@ -12,36 +15,98 @@ type WorkoutProps = {
 };
 
 export default function WorkoutForm({ onSubmit }: WorkoutProps) {
-  const [form, setForm] = useState({
-    name: "",
-    duration: "",
-  });
-
-  const onChageText = (name: string) => (text: string) => {
-    setForm({
-      ...form,
-      [name]: text,
-    });
-  };
+  const { control, handleSubmit } = useForm();
+  const [isSelectionOn, setSelectionOn] = useState(false);
+  const selectionItem: string[] = ["Exercise", "break", "stretch"];
 
   return (
     <View style={styles.container}>
       <Text>Exercise Form</Text>
       <View>
-        <TextInput
-          onChangeText={onChageText("name")}
-          style={styles.input}
-          value={form.name}
-        />
-        <TextInput
-          onChangeText={onChageText("duration")}
-          style={styles.input}
-          value={form.duration}
-        />
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            rules={{ required: true }}
+            name="name"
+            defaultValue={""}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Enter a exercise name"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{ required: true }}
+            name="duration"
+            defaultValue={""}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Enter a duration"
+              />
+            )}
+          />
+        </View>
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            rules={{ required: true }}
+            name="type"
+            defaultValue={""}
+            render={({ field: { onChange, value } }) => (
+              <View>
+                {isSelectionOn ? (
+                  <View>
+                    {selectionItem.map((selection) => (
+                      <PressableText
+                        style={styles.selection}
+                        key={selection}
+                        text={selection}
+                        onPress={() => {
+                          onChange(selection);
+                          setSelectionOn(false);
+                        }}
+                      />
+                    ))}
+                  </View>
+                ) : (
+                  <TextInput
+                    onFocus={() => setSelectionOn(true)}
+                    style={styles.input}
+                    value={value}
+                    placeholder="Enter a type"
+                  />
+                )}
+              </View>
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{ required: true }}
+            name="reps"
+            defaultValue={""}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Enter reps"
+              />
+            )}
+          />
+        </View>
         <PressableText
-          onPress={() => onSubmit(form)}
+          onPress={handleSubmit((data) => {
+            onSubmit(data as ExerciseForm);
+          })}
           text="Submit"
-        ></PressableText>
+        />
       </View>
     </View>
   );
@@ -49,17 +114,25 @@ export default function WorkoutForm({ onSubmit }: WorkoutProps) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     margin: 20,
     padding: 10,
     backgroundColor: "white",
     borderRadius: 10,
   },
   input: {
+    flex: 1,
     height: 40,
     margin: 12,
     padding: 10,
     borderWidth: 1,
     borderRadius: 10,
+  },
+  rowContainer: {
+    flexDirection: "row",
+  },
+  selection: {
+    margin: 2,
+    padding: 3,
+    alignSelf: "center",
   },
 });
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -3,7 +3,7 @@ import WorkoutForm, { ExerciseForm } from "../components/WorkoutForm";
 
 export default function PlannerScreen({ navigation }: any) {
   const handleFormSubmit = (form: ExerciseForm) => {
-    alert(`${form.name} ${form.duration}`);
+    alert(`${form.name} ${form.duration} ${form.type} ${form.reps}`);
   };
   return (
     <View>
